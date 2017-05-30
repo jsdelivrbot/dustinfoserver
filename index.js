6aa3e96e589e948142c9f4ccc543d77ddfe3d839
stylistbong1 <stylistbong1@gmail.com>
2017-05-30 22:37:23 +0900

    parsing dust info test

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
-var express = require('express');
-var app = express();
+var badDustContition = 80;
+var get_request = require("request");
 
-app.set('port', (process.env.PORT || 5000));
+var fetch_options = {
+    url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName=%EC%84%9C%EC%9A%B8&searchCondition=DAILY&pageNo=1&numOfRows=10&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json',
+}
 
-app.use(express.static(__dirname + '/public'));
+function fetch_callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //console.log(body);
+      var info = JSON.parse(body);
+      var list = info.list;
+      var cityName = list[0].cityName;  
+      var dataTime = list[0].dataTime;
+      var pm10Value = list[0].pm10Value;
+      var pm25Value = list[0].pm25Value;
+      console.log(cityName+' '+dataTime+' PM10: '+pm10Value+' PM2.5: '+pm25Value);
+    }
+}
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+function startDustObserving() {
+    var isTriggered = false;
 
-app.get('/', function(request, response) {
-  response.render('pages/index', { title: 'Heroku' });
-  console.log('hello heroku ');
-});
+    function triggerGetRequest() {
+        get_request(fetch_options, fetch_callback);
+    }
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+    function callEveryHour() {
+        if (isTriggered == false) {
+            triggerGetRequest();
+            isTriggered = true;     
+        }
+        setInterval(triggerGetRequest, 1000 * 60 * 60);
+    }
 
+    get_request(fetch_options, fetch_callback);
 
+    var nextDate = new Date();
+    var d = nextDate;
+
+    if (nextDate.getMinutes() === 0) {
+        callEveryHour();
+    } else if (nextDate.getMinutes() < 20) {
+        nextDate.setHours(d.getHours());
+        nextDate.setMinutes(5);
+        nextDate.setSeconds(0);
+        var difference = nextDate - new Date();
+        setTimeout(callEveryHour, difference);
+    }
+    else {
+        nextDate.setHours(d.getHours() + 1);
+        nextDate.setMinutes(5);
+        nextDate.setSeconds(0);
+        var difference = nextDate - new Date();
+        setTimeout(callEveryHour, difference);
+    }
+}
+
+startDustObserving();
