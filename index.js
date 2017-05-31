d931e7c4983fc12ae8f69c20f9fdd5351ba7344e
stylistbong1 <stylistbong1@gmail.com>
2017-06-01 01:00:12 +0900

    add full sido dust info

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var get_request = require("request");
 var express = require('express');
 var app = express();
-var list;
+var list = new Array(17);
+var sidoList = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주', '세종'];
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,13 +16,19 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/getDust/:cityname', function (req, res) {
-  console.log('app.get: '+req.params.cityname);
+app.get('/getDust/sido/:sidoname', function (req, res) {
+  console.log('app.get: '+req.params.sidoname);
+  var indexOfsidoName = sidoList.indexOf(req.params.sidoname);
+  res.json(list[indexOfsidoName]);
+});
 
+app.get('/getDust/sido/:sidoname/:cityname', function (req, res) {
+  console.log('app.get: '+req.params.sidoname+' - '+req.params.cityname);
+  var indexOfsidoName = sidoList.indexOf(req.params.sidoname);
   var _list = new Array();
-  for(var i in list) {
-      if(list[i].cityName == req.params.cityname) {
-        _list.push(list[i]);
+  for(var i in list[indexOfsidoName]) {
+      if(list[indexOfsidoName][i].cityName == req.params.cityname) {
+        _list.push(list[indexOfsidoName][i]);
       }
   }
   res.json(_list);
@@ -31,30 +38,50 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
-var fetch_options = {
-    url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName=%EC%84%9C%EC%9A%B8&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json',
-}
+var fetch_options = [
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[0])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[1])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[2])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[3])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[4])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[5])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[6])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[7])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[8])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[9])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[10])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[11])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[12])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[13])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[14])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[15])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'},
+  {url: 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName='+encodeURI(sidoList[16])+'&searchCondition=DAILY&pageNo=1&numOfRows=1000&ServiceKey=fSjF%2BbianLiW4jHvdfbA01kU%2F3KswrUWX87iXUYtqDdkDWqv2W8FSEmVZTob203aERdrQ%2BxOZD7mmc1Q%2FTGwgQ%3D%3D&_returnType=json'}]
 
 function fetch_callback(error, response, body) {
     if (!error && response.statusCode == 200) {
       //console.log(body);
       var info = JSON.parse(body);
-      list = info.list;
-
-      //var cityName = list[0].cityName;  
-      //var dataTime = list[0].dataTime;
-      //var pm10Value = list[0].pm10Value;
-      //var pm25Value = list[0].pm25Value;
-      console.log('Save List : ' + new Date());
+      //console.log('Retuen : '  + info);
+      var indexOfsidoName = sidoList.indexOf(info.list[0].sidoName);
+      //console.log(info.list[0].sidoName +" - "+indexOfsidoName);
+      list[indexOfsidoName] = info.list;
+      //console.log('Save List : '+ list[0][0].sidoName);
     }
+}
+
+function get_full_request () {
+  for( var i in sidoList) {
+    //console.log('get_full_request:' + i);
+    get_request(fetch_options[i], fetch_callback);
+  }
 }
 
 function startDustObserving() {
     var isTriggered = false;
 
     function triggerGetRequest() {
-        get_request(fetch_options, fetch_callback);
+        //get_request(fetch_options, fetch_callback);
+        get_full_request();
     }
 
     function callEveryHour() {
@@ -62,26 +89,27 @@ function startDustObserving() {
             triggerGetRequest();
             isTriggered = true;     
         }
-        setInterval(triggerGetRequest, 1000 * 60 * 30);
+        setInterval(triggerGetRequest, 1000 * 60 * 60);
     }
 
-    get_request(fetch_options, fetch_callback);
+    //get_request(fetch_options[7], fetch_callback);
+    get_full_request();
 
     var nextDate = new Date();
     var d = nextDate;
 
-    if (nextDate.getMinutes() === 0) {
+    if (nextDate.getMinutes() === 30) {
         callEveryHour();
-    } else if (nextDate.getMinutes() < 10) {
+    } else if (nextDate.getMinutes() < 30) {
         nextDate.setHours(d.getHours());
-        nextDate.setMinutes(10);
+        nextDate.setMinutes(30);
         nextDate.setSeconds(0);
         var difference = nextDate - new Date();
         setTimeout(callEveryHour, difference);
     }
     else {
         nextDate.setHours(d.getHours() + 1);
-        nextDate.setMinutes(10);
+        nextDate.setMinutes(30);
         nextDate.setSeconds(0);
         var difference = nextDate - new Date();
         setTimeout(callEveryHour, difference);
