ca61e79ee6fda236a0dca39dac8e0a20ab66fb1e
stylistbong1 <stylistbong1@gmail.com>
2017-06-01 20:10:30 +0900

    add default path return json

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.json({msg: 'OK' });
 });
 
 app.get('/getDust/sido/:sidoname', function (req, res) {
